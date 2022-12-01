b82709c2bb4d875f31addeb4e3b42e78346b0bdc
gokite227 <102020249+gokite227@users.noreply.github.com>
2022-12-01 19:48:40 +0900

    unit test2

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -7,6 +7,8 @@ const chaiBN = require('chai-bn')(BN);
 chai.use(chaiBN);
 
 var chaiAsPromised = require("chai-as-promised");
+const exp = require("constants");
+
 chai.use(chaiAsPromised);
 
 const expect = chai.expect;
@@ -18,5 +20,23 @@ contract("Token Test", async accounts => {
         let instance = await Token.deployed();
         let totalSupply = await instance.totalSupply();
         await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
+    });
+
+    it("I can send tokens from Account 1 to Account 2", async () => {
+      const sendTokens = 1;
+      let instance = await Token.deployed();
+      let totalSupply = await instance.totalSupply();
+      await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
+      await expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;      
+      await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
+      await expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+    });
+
+    it("It's not possible to send tokens than account 1 has", async () => {
+        let instance = await Token.deployed();
+        let balanceOfAccount = await instance.balanceOf(initialHolder);
+        await expect(instance.transfer(recipient, new BN(balanceOfAccount+1))).to.eventually.be.rejected;
+
+        await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(balanceOfAccount);
     });
 });
